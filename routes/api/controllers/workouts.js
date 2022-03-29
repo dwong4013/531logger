41f05d22db48da1b7197a8673e2aec5ad1b44701
derrick <dwong4013@gmail.com>
2022-03-29 13:20:08 -0400

    created workout module to create workout documents accompanied by unit tests

--- routes/api/controllers/workouts.js
+++ routes/api/controllers/workouts.js
@@ -19,6 +19,67 @@ const getWorkouts = async (req, res) => {
     }
 }
 
+const createWorkouts = async (req, res) => {
+    // Find the cycle to get the maxes
+
+    // Create the workout based on the following:
+    // Maxes from cycle
+    // 3 exercises
+    // 3 weeks per exercise
+    // Each workout has 3 main sets and 5 volume sets
+
+    try {
+        let cycle = Cycle.findById(cycle_id)
+
+        if (!cycle) {
+            return res
+            .status(400)
+            .json({ msg: 'Failed to generate cycle.' });
+        }
+
+        let exercises = [ 'squat', 'bench', 'deadlift', 'press']
+        let { maxes: {squatMax, benchMax, deadliftMax, pressMax } } = cycle
+        let newWorkouts = [];
+
+        
+        Workout.insertMany()
+
+        return res.status(200).json({
+            msg: 'A new cycle has been created!'
+        });
+    } catch (err) {
+        return res.status(500).send('Server Error');
+    }
+}
+
+const editWorkout = async (req, res) => {
+  const { cycle_id } = req.params;
+  const { key, value } = req.body;
+
+  try {
+    const cycle = await Cycle.findById({
+      _id: cycle_id,
+      user: req.user.id
+    });
+
+    cycle[key] = value;
+
+    await cycle.save();
+
+    const cycles = await Cycle.find({
+      user: req.user.id
+    }).sort({ dateCreated: 'desc' });
+
+    return res.json(cycles);
+  } catch (err) {
+    if (err.kind && err.kind === undefined) {
+      return res.status(400).json({msg: 'Invalid cycle.'})
+    }
+
+    return res.status(500).send('Server Error');
+  }
+}
+
 
 module.exports = {
     getWorkouts,
--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -15,6 +15,26 @@ router.get('/', auth, getCycles);
 // @desc    Create a cycle
 // @access  Private
 
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('squat', 'Please enter a squat max.').isNumeric().not().isEmpty(),
+      check('bench', 'Please enter a bench max.').isNumeric().not().isEmpty(),
+      check('deadlift', 'Please enter a deadlift max.').isNumeric().not().isEmpty(),
+      check('press', 'Please enter a press max.').isNumeric().not().isEmpty()
+    ],
+    validationHandler
+  ],
+  createCycle
+);
+
+// @route   Edit api/cycle/:cycle_id
+// @desc    Edit cycle id
+// @access  Private
+
+router.put('/:cycle_id', auth, editCycle);
 
 
 module.exports = router;
