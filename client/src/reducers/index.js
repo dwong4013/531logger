af0b5585a445ac73f1f47fbcaef9e72b3c65f905
derrick <dwong4013@gmail.com>
2022-03-31 12:31:51 -0400

    removed main, maxes, and volume action creators and reducers, and removed from root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,16 +1,10 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
-import maxes from './maxes';
-import volume from './volume';
 import cycles from './cycles';
-import main from './main';
 
 export default combineReducers({
   alert,
   auth,
-  main,
-  maxes,
-  volume,
   cycles
 });
