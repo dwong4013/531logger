8b5d4ac01954af03d63ef262bcf52d4eae77ab38
derrick <dwong4013@gmail.com>
2022-04-05 12:54:23 -0400

    removed validation error handling from register route controller, and added validationHandler middleware to the user register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 const { registerUser } = require('./controllers/users');
+const { validationHandler } = require('../../middleware/validationHandler')
 
 
 // @route   POST api/users
@@ -10,14 +11,14 @@ const { registerUser } = require('./controllers/users');
 
 router.post(
   '/',
-  [
+  [[
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Email is required').isEmail(),
     check(
       'password',
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
-  ],
+  ], validationHandler],
   registerUser
 );
 
