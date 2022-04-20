21fd4fc0c3c7bf0e161867d5c786b32b832403e1
derrick <dwong4013@gmail.com>
2022-04-20 14:55:59 -0400

    updated Modal to create modal element if it doesn't already exist

--- client/src/components/modals/Modal.js
+++ client/src/components/modals/Modal.js
@@ -2,14 +2,24 @@ import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 export default function Modal({children}) {
-    const mount = document.getElementById('modal');
+    let modalRoot = document.getElementById('modal');
+    // if there is no modal element in index.html, 
+    // it will be created and appended to document.body
+    if (!modalRoot) {
+      modalRoot = document.createElement('div');
+      modalRoot.setAttribute('id', 'modal')
+      document.body.appendChild(modalRoot);
+    }
+
+    // // Only use create a new element and mount to it if your portal is not a dynamic component. The component changes will cause re-render
+    // const el = document.createElement('div');
 
     // useEffect(() => {
-    //     mount.appendChild(children)
-    //     return () => mount.removeChild(children)
-    // },[mount])
+    //     modalRoot.appendChild(el)
+    //     return () => modalRoot.removeChild(el)
+    // },[])
 
   return (
-    ReactDOM.createPortal(children, mount)
+    ReactDOM.createPortal(children, modalRoot)
   )
 }
