2cd91f2ae5d3bb1bce8c3e23f7e93b2a21a2c45a
derrick <dwong4013@gmail.com>
2022-04-20 16:34:19 -0400

    refactored cycle form and added custom validation to require numbers

--- client/src/components/forms/Input.js
+++ client/src/components/forms/Input.js
@@ -4,8 +4,8 @@ import React, { Fragment } from 'react';
 export default function Input({name, register, validation, errors, ...rest}) {
   return (
       <Fragment>
-        <input {...register(name, validation)} {...rest} />
-        {errors[name] && <p className="text-primary text-small">{errors[name].message}</p>}
+        <input {...register(name, validation)} label={name} {...rest} />
+        {errors[name] && <p className="text-primary text-small">{validation.messages[errors[name].type]}</p>}
       </Fragment>
   )
 }
