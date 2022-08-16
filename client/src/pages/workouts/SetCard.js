e1b5d4130c4b3ecfb7ecbba5555b93a8af71ab99
derrick <dwong4013@gmail.com>
2022-08-16 09:43:47 -0400

    reformatted workout pages

--- client/src/pages/workouts/SetCard.js
+++ client/src/pages/workouts/SetCard.js
@@ -1,37 +1,48 @@
-import React, { Fragment, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import React, { Fragment, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
-export default function SetCard ({id, set, displayNotes}) {
-  const  { weight, reps, time, notes } = set;
+export default function SetCard({ id, set, displayNotes }) {
+  const { weight, reps, time, notes } = set;
   // View notes toggle state
-  const [ toggled, setToggled ] = useState(false);
+  const [toggled, setToggled] = useState(false);
 
   // Toggle note
   const handleToggle = () => {
-    setToggled(!toggled)
-  }
+    setToggled(!toggled);
+  };
   return (
     <Fragment>
       <div className="set-card set-card-container">
         <div className="set-card-details">
-          <p className="text-dark text-small">{`set ${id +1}`}</p>
+          <p className="text-dark text-small">{`set ${id + 1}`}</p>
           <p className="text-dark text-regular text-bold">{`${weight} lbs`}</p>
           <p className="text-dark text-regular text-bold">{`${reps} reps`}</p>
           <p className="text-primary text-small">{time && time}</p>
-          {displayNotes && <p className="text-dark text-small">{notes && notes}</p>}
-          <FontAwesomeIcon icon={solid('ellipsis')} size='2x' />
+          {displayNotes && (
+            <p className="text-dark text-small">{notes && notes}</p>
+          )}
+          <FontAwesomeIcon icon={solid("ellipsis")} size="2x" />
         </div>
-        {!displayNotes && notes && <div className={`notes-container ${toggled && `notes-container-toggled`}`}>
-          {toggled && <p className="notes text-dark text-small">{notes && notes}</p>}
-          <div onClick={() => handleToggle()}>
-            {!toggled ? <FontAwesomeIcon icon={solid('caret-down')} /> : 
-            <FontAwesomeIcon icon={solid('caret-up')}/>}
-          </div> 
-        </div>}
-        </div>
+        {!displayNotes && notes && (
+          <div
+            className={`notes-container ${
+              toggled && `notes-container-toggled`
+            }`}
+          >
+            {toggled && (
+              <p className="notes text-dark text-small">{notes && notes}</p>
+            )}
+            <div onClick={() => handleToggle()}>
+              {!toggled ? (
+                <FontAwesomeIcon icon={solid("caret-down")} />
+              ) : (
+                <FontAwesomeIcon icon={solid("caret-up")} />
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </Fragment>
   );
-};
-
-
+}
--- client/src/pages/workouts/Workout.js
+++ client/src/pages/workouts/Workout.js
@@ -1,52 +1,63 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import Spinner from '../../components/layout/Spinner';
-import { getWorkout, editWorkout } from '../../actions/workouts';
-import { getCycles } from '../../actions/cycles';
-import SetCard from './SetCard';
-import SummaryCard from '../../pages/dashboard/SummaryCard';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import React, { Fragment, useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../../components/layout/Spinner";
+import { getWorkout, editWorkout } from "../../actions/workouts";
+import { getCycles } from "../../actions/cycles";
+import SetCard from "./SetCard";
+import SummaryCard from "../../pages/dashboard/SummaryCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 // Prevent fontawesome from adding css to the head
 config.autoAddCss = false;
 
 const capitalize = (string) => {
-  return string[0].toUpperCase()+string.slice(1);
-}
+  return string[0].toUpperCase() + string.slice(1);
+};
 
 const initialCurrentSetState = {
   setIndex: 0,
-  setType: 'mainSets',
+  setType: "mainSets",
   setData: {
-    id: '',
-    weight: '',
-    reps: '',
-    notes: '',
-    time: '',
-  }
-}
+    id: "",
+    weight: "",
+    reps: "",
+    notes: "",
+    time: "",
+  },
+};
 
-const Workout = ({ getWorkout, editWorkout, getCycles, cycles, workouts, match }) => {
+const Workout = ({
+  getWorkout,
+  editWorkout,
+  getCycles,
+  cycles,
+  workouts,
+  match,
+}) => {
   // Listen to screen width changes
   useEffect(() => {
-    window.matchMedia('(max-width: 414px)').addEventListener('change', mediaHandler)
+    window
+      .matchMedia("(max-width: 414px)")
+      .addEventListener("change", mediaHandler);
   }, []);
 
-  const [desktop, setDesktop] = useState(window.matchMedia('(max-width: 414px)').matches)
+  const [desktop, setDesktop] = useState(
+    window.matchMedia("(max-width: 414px)").matches
+  );
 
-  const mediaHandler = e => {
-    setDesktop(e.matches)
-  }
+  const mediaHandler = (e) => {
+    setDesktop(e.matches);
+  };
 
   // Load cycle data
   useEffect(() => {
-    getCycles()
-  },[getCycles])
+    getCycles();
+  }, [getCycles]);
 
   // Destructure state
   const { workout, loading: workoutLoading } = workouts;
@@ -55,134 +66,200 @@ const Workout = ({ getWorkout, editWorkout, getCycles, cycles, workouts, match }
 
   // Hooks
   // selecting workout
-  const [ targetWorkout, setTargetWorkout ] = useState(null)
+  const [targetWorkout, setTargetWorkout] = useState(null);
   // tracking remaining sets
-  const [ remainingSets, setRemainingSets ] = useState(null)
+  const [remainingSets, setRemainingSets] = useState(null);
   // tracking current set
-  const [ currentSet, setCurrentSet ] = useState(initialCurrentSetState)
+  const [currentSet, setCurrentSet] = useState(initialCurrentSetState);
   // notes field
-  const [ notes, setNotes ] = useState('')
-  
+  const [notes, setNotes] = useState("");
+
   // Load workout with or without targetWorkout id
   useEffect(() => {
-    if(targetWorkout) {
-      getWorkout(cycleId, targetWorkout)
-    } else{ 
-      getWorkout(cycleId)
+    if (targetWorkout) {
+      getWorkout(cycleId, targetWorkout);
+    } else {
+      getWorkout(cycleId);
     }
   }, [targetWorkout, cycleId, getWorkout]);
-  
+
   // Set remainingSets state when workout is updated
   useEffect(() => {
     if (workout) {
       // only take elements in the array that have a false value for completed and missed
       setRemainingSets({
-        mainSets: [...workout.mainSets].filter(set => set.completed === false && set.missed === false),
-        volumeSets: [...workout.volumeSets].filter(set => set.completed === false && set.missed === false)})
+        mainSets: [...workout.mainSets].filter(
+          (set) => set.completed === false && set.missed === false
+        ),
+        volumeSets: [...workout.volumeSets].filter(
+          (set) => set.completed === false && set.missed === false
+        ),
+      });
     }
-  },[workout])
+  }, [workout]);
 
   // Set currentSet state when remainingSets state is updated
   useEffect(() => {
     if (remainingSets) {
       // If there are any remainingSets left, then set the currentSet
-      if (remainingSets.mainSets.length > 0 || remainingSets.volumeSets.length > 0) {
+      if (
+        remainingSets.mainSets.length > 0 ||
+        remainingSets.volumeSets.length > 0
+      ) {
         // Determine setIndex based on setType
-        let setType = remainingSets.mainSets.length > 0 ? 'mainSets': 'volumeSets'
-        let expectedSetLength = setType === 'mainSets' ? 3 : 5
-        let setIndex = expectedSetLength - remainingSets[setType].length
+        let setType =
+          remainingSets.mainSets.length > 0 ? "mainSets" : "volumeSets";
+        let expectedSetLength = setType === "mainSets" ? 3 : 5;
+        let setIndex = expectedSetLength - remainingSets[setType].length;
         // set the currentSet state with new setType, setIndex
         // setData is always filled with the first set in the remainingSets[setType] array
-        setCurrentSet((currentSet) => ({...currentSet,
-          setIndex, 
+        setCurrentSet((currentSet) => ({
+          ...currentSet,
+          setIndex,
           setType,
           setData: {
             id: remainingSets[setType][0]._id,
             weight: remainingSets[setType][0].weight,
             reps: remainingSets[setType][0].reps,
-        }
-      }))
+          },
+        }));
       } else {
         // Set the currentSet back to inital state when the workout is complete
         setCurrentSet(initialCurrentSetState);
       }
     }
-  },[remainingSets])
+  }, [remainingSets]);
 
   const onWorkoutSelect = (e) => {
-    setTargetWorkout(e.target.value)
-  }
+    setTargetWorkout(e.target.value);
+  };
 
   const onNotesChange = (e) => {
-    setNotes(e.target.value)
-  }
+    setNotes(e.target.value);
+  };
 
   const updateSet = (outcome) => {
     let formData = {
-      type: 'edit',
+      type: "edit",
       values: {
         ...currentSet.setData,
         id: currentSet.setData.id,
         setType: currentSet.setType,
         [outcome]: true,
         time: new Date().toLocaleTimeString(),
-        notes
-      }
-    }
-    // If it is the last element in the volumeSets array, 
+        notes,
+      },
+    };
+    // If it is the last element in the volumeSets array,
     // then add workoutCompleted to the values to complete the workout
-    if (currentSet.setType === 'volumeSets' && currentSet.setIndex === 4) {
+    if (currentSet.setType === "volumeSets" && currentSet.setIndex === 4) {
       formData.values.workoutCompleted = true;
     }
-    editWorkout(workout._id,formData)
-    setNotes('')
-  }
+    editWorkout(workout._id, formData);
+    setNotes("");
+  };
 
   return (
     <Fragment>
-    {workoutLoading && 
-    cyclesLoading ? (
-      <Spinner/>
-    ) : (
-      <section className="summary-container container-flex container-vertical container-vertical-center px-6">
-        <div className="toolbar">
-        <Link to='/dashboard' className="btn btn-back btn-icon-left btn-small btn-dark toolbar-left"><FontAwesomeIcon icon={solid('caret-left')} /> back</Link>
-        <select onChange={e=> onWorkoutSelect(e)} className="select toolbar-right" name="pets" id="pet-select">
-          {workout && currentCycle && currentCycle.workoutsToDo.map((workoutOption) => (
-            <option key={workoutOption._id} value={`${workoutOption._id}`} defaultValue={workoutOption._id === workout._id }>{`Week ${workoutOption.week}: ${capitalize(workoutOption.exercise)}`}</option>
-          ))}
-        </select>
-        </div>
-        {/* Summary Cards */}
-        {workout && !workout.completed ? (<div className="summary-cards-container ">
-          <SummaryCard title={workout && `${workout.exercise} max`} value={`${currentCycle && workout && currentCycle.maxes[workout.exercise]}`}/>
-          <SummaryCard light title='set' value={currentSet.setIndex + 1}/>
-          <SummaryCard light={desktop} title='weight' value={currentSet.setData.weight}/>
-          <SummaryCard light={!desktop} title='reps' value={currentSet.setData.reps}/>
-        </div>) :(
-          <p className="text-primary text-regular my-1">Workout Completed!</p>
-        )}
-        {/* Actions */}
-        {workout && !workout.completed && <div className="set-actions">
-          <form>
-            <input onChange={e=>onNotesChange(e)} name='notes' value={notes} type="text" placeholder="notes for current set"/>
-          </form>
-          <div className="buttons container-flex container-horizontal">
-            <button onClick={() => updateSet('missed')} className="btn btn-regular btn-dark"> missed </button>
-            <button onClick={() => updateSet('completed')}className="btn btn-regular btn-primary"> complete </button>
+      {workoutLoading && cyclesLoading ? (
+        <Spinner />
+      ) : (
+        <section className="summary-container container-flex container-vertical container-vertical-center px-6">
+          <div className="toolbar">
+            <Link
+              to="/dashboard"
+              className="btn btn-back btn-icon-left btn-small btn-dark toolbar-left"
+            >
+              <FontAwesomeIcon icon={solid("caret-left")} /> back
+            </Link>
+            <select
+              onChange={(e) => onWorkoutSelect(e)}
+              className="select toolbar-right"
+              name="pets"
+              id="pet-select"
+            >
+              {workout &&
+                currentCycle &&
+                currentCycle.workoutsToDo.map((workoutOption) => (
+                  <option
+                    key={workoutOption._id}
+                    value={`${workoutOption._id}`}
+                    defaultValue={workoutOption._id === workout._id}
+                  >{`Week ${workoutOption.week}: ${capitalize(
+                    workoutOption.exercise
+                  )}`}</option>
+                ))}
+            </select>
           </div>
-        </div>}
-        <p className="text-small text-dark text-bold my-1">Main Sets</p>
-        {/* Main Sets */}
-        {workout && workout.mainSets.map((set, id) => (
-          <SetCard key={id} displayNotes={!desktop} id={id} set={set}/>
-        ))}
-        <p className="text-small text-dark text-bold my-1">Volume Sets</p>
-        {workout && workout.volumeSets.map((set, id) => (
-          <SetCard key={id} displayNotes={!desktop} id={id} set={set}/>
-        ))}
-      </section>
-    )}
+          {/* Summary Cards */}
+          {workout && !workout.completed ? (
+            <div className="summary-cards-container ">
+              <SummaryCard
+                title={workout && `${workout.exercise} max`}
+                value={`${
+                  currentCycle &&
+                  workout &&
+                  currentCycle.maxes[workout.exercise]
+                }`}
+              />
+              <SummaryCard light title="set" value={currentSet.setIndex + 1} />
+              <SummaryCard
+                light={desktop}
+                title="weight"
+                value={currentSet.setData.weight}
+              />
+              <SummaryCard
+                light={!desktop}
+                title="reps"
+                value={currentSet.setData.reps}
+              />
+            </div>
+          ) : (
+            <p className="text-primary text-regular my-1">Workout Completed!</p>
+          )}
+          {/* Actions */}
+          {workout && !workout.completed && (
+            <div className="set-actions">
+              <form>
+                <input
+                  onChange={(e) => onNotesChange(e)}
+                  name="notes"
+                  value={notes}
+                  type="text"
+                  placeholder="notes for current set"
+                />
+              </form>
+              <div className="buttons container-flex container-horizontal">
+                <button
+                  onClick={() => updateSet("missed")}
+                  className="btn btn-regular btn-dark"
+                >
+                  {" "}
+                  missed{" "}
+                </button>
+                <button
+                  onClick={() => updateSet("completed")}
+                  className="btn btn-regular btn-primary"
+                >
+                  {" "}
+                  complete{" "}
+                </button>
+              </div>
+            </div>
+          )}
+          <p className="text-small text-dark text-bold my-1">Main Sets</p>
+          {/* Main Sets */}
+          {workout &&
+            workout.mainSets.map((set, id) => (
+              <SetCard key={id} displayNotes={!desktop} id={id} set={set} />
+            ))}
+          <p className="text-small text-dark text-bold my-1">Volume Sets</p>
+          {workout &&
+            workout.volumeSets.map((set, id) => (
+              <SetCard key={id} displayNotes={!desktop} id={id} set={set} />
+            ))}
+        </section>
+      )}
     </Fragment>
   );
 };
@@ -192,12 +269,12 @@ Workout.propTypes = {
   cycles: PropTypes.object.isRequired,
   getWorkout: PropTypes.func.isRequired,
   editWorkout: PropTypes.func.isRequired,
-  getCycles: PropTypes.func.isRequired
+  getCycles: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   workouts: state.workouts,
-  cycles: state.cycles
+  cycles: state.cycles,
 });
 
 export default connect(mapStateToProps, { getWorkout, editWorkout, getCycles })(
