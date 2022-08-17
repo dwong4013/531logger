d4ebd4d220ea371bab8f2f147c66ac5913044e67
derrick <dwong4013@gmail.com>
2022-08-17 14:32:49 -0400

    reformatted page tests

--- client/src/pages/__tests__/cycles.js
+++ client/src/pages/__tests__/cycles.js
@@ -1,180 +1,190 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect'
-import { screen } from '@testing-library/dom'
-import { render, fireEvent, waitForElement } from '@testing-library/react';
-import axios from 'axios';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { screen } from "@testing-library/dom";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+import axios from "axios";
 
 // Components
-import App from '../../App';
+import App from "../../App";
 
-describe('Dashboard', () => {
-  describe('login and createa a cycle', () => {
+describe("Dashboard", () => {
+  describe("login and createa a cycle", () => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
 
-    Object.defineProperty(window, 'matchMedia', {
-        writable: true,
-        value: jest.fn().mockImplementation(query => ({
-          matches: false,
-          media: query,
-          onchange: null,
-          addListener: jest.fn(), // deprecated
-          removeListener: jest.fn(), // deprecated
-          addEventListener: jest.fn(),
-          removeEventListener: jest.fn(),
-          dispatchEvent: jest.fn(),
-        })),
-      });
-    
     const renderApp = () => {
-      let utils = render(<App/>)
-      return {...utils, history}
-    }
+      let utils = render(<App />);
+      return { ...utils, history };
+    };
 
     const res = {
       postAuthSuccess: {
-        data: 'asdofj1091jjksladf01asdf'
+        data: "asdofj1091jjksladf01asdf",
       },
       getAuth: {
         data: {
-          _id: '0s9adf808sadfa0sdf8',
-          name: 'fake',
-          email: 'fake@mail.com',
+          _id: "0s9adf808sadfa0sdf8",
+          name: "fake",
+          email: "fake@mail.com",
           cyclesCompleted: 0,
-        }
+        },
       },
       getCycles: {
         response: {
-            data: {
-                status: 400,
-                error: {
-                    msg: 'No cycles found'
-                }
-            }
-        }
+          data: {
+            status: 400,
+            error: {
+              msg: "No cycles found",
+            },
+          },
+        },
       },
       postCycle: {
         data: {
-            cycle: {
-                _id: '62607be5ab7521076f156d33',
-                user: '624de9fa6926a5051d0cf9b0',
-                completed: false,
-                workoutsToDo: [],
-                workoutsCompleted: [],
-                maxes: {
-                    squat: 200,
-                    bench: 150,
-                    deadlift: 300,
-                    press: 100
-                }
+          cycle: {
+            _id: "62607be5ab7521076f156d33",
+            user: "624de9fa6926a5051d0cf9b0",
+            completed: false,
+            workoutsToDo: [],
+            workoutsCompleted: [],
+            maxes: {
+              squat: 200,
+              bench: 150,
+              deadlift: 300,
+              press: 100,
             },
-            msg: 'A new cycle has been created'
-        }
+          },
+          msg: "A new cycle has been created",
+        },
       },
       postWorkouts: {
-          data: {
-              workouts: ['1','2','3','4','5']
-          }
+        data: {
+          workouts: ["1", "2", "3", "4", "5"],
+        },
       },
       putCycle: {
         data: {
-            _id: '62607be5ab7521076f156d33',
-            user: '624de9fa6926a5051d0cf9b0',
-            completed: false,
-            workoutsToDo: ['1','2','3','4','5'],
-            workoutsCompleted: [],
-            maxes: {
-                squat: 200,
-                bench: 150,
-                deadlift: 300,
-                press: 100
-            }
-        }
-      }
-    }
-    beforeEach(() => jest.restoreAllMocks())
-    afterEach(() => jest.restoreAllMocks())
-  
-    test('login and create a new cycle', async () => {
-      jest.setTimeout(20000);
-        // Mock loaduser, getCycles
-        axios.get = jest.fn()
-        .mockImplementation((url) => {
-            switch(url) {
-                case '/api/auth':
-                    return Promise.resolve(res.getAuth)
-                case '/api/cycles':
-                    return Promise.reject(res.getCycles);
-            }
-        });
-
-        // Render Landing and Register Routes, start at Landing
-        const { getByText, getByTestId, getByPlaceholderText, getAllByPlaceholderText, getByRole, history } = renderApp();
-
-        // See buttons to login or register
-        expect(getByText(/login/i)).toBeTruthy();
-        expect(getByText(/register/i)).toBeTruthy();
-
-        // Navigate to Login
-        const leftClick = { button: 0 }
-        fireEvent.click(getByText(/login/i), leftClick)
-
-        // Redirected to dashboard
-        await waitForElement(() => getByText(/logout/i));
-        expect(window.location.href).toContain('http://localhost/dashboard');
-
-        // See no cycles
-        expect(getByText(/no cycles available/i)).toBeTruthy();
-
-        // See add (+) button
-        expect(getByTestId('create-cycle-button')).toBeTruthy();
-        fireEvent.click(getByTestId('create-cycle-button'), leftClick);
-
-        // See modal
-        await waitForElement(() => getByText(/let\'s setup your maxes/i))
-        expect(getByRole('button', {name: /setup/i})).toBeTruthy();
-
-        fireEvent.click(getByRole('button', {name: /setup/i}), leftClick);
-
-        // See form to enter maxes
-        const maxes = {
+          _id: "62607be5ab7521076f156d33",
+          user: "624de9fa6926a5051d0cf9b0",
+          completed: false,
+          workoutsToDo: ["1", "2", "3", "4", "5"],
+          workoutsCompleted: [],
+          maxes: {
             squat: 200,
             bench: 150,
             deadlift: 300,
-            press: 100
+            press: 100,
+          },
+        },
+      },
+    };
+    beforeEach(() => jest.restoreAllMocks());
+    afterEach(() => jest.restoreAllMocks());
+
+    test("login and create a new cycle", async () => {
+      jest.setTimeout(20000);
+      // Mock loaduser, getCycles
+      axios.get = jest.fn().mockImplementation((url) => {
+        switch (url) {
+          case "/api/auth":
+            return Promise.resolve(res.getAuth);
+          case "/api/cycles":
+            return Promise.reject(res.getCycles);
         }
-        for (const exercise in maxes) {
-            expect(getByRole('textbox', {name: new RegExp(exercise,'i')})).toBeTruthy();
+      });
+
+      // Render Landing and Register Routes, start at Landing
+      const {
+        getByText,
+        getByTestId,
+        getByPlaceholderText,
+        getAllByPlaceholderText,
+        getByRole,
+        history,
+      } = renderApp();
+
+      // See buttons to login or register
+      expect(getByText(/login/i)).toBeTruthy();
+      expect(getByText(/register/i)).toBeTruthy();
+
+      // Navigate to Login
+      const leftClick = { button: 0 };
+      fireEvent.click(getByText(/login/i), leftClick);
+
+      // Redirected to dashboard
+      await waitForElement(() => getByText(/logout/i));
+      expect(window.location.href).toContain("http://localhost/dashboard");
+
+      // See no cycles
+      expect(getByText(/no cycles available/i)).toBeTruthy();
+
+      // See add (+) button
+      expect(getByTestId("create-cycle-button")).toBeTruthy();
+      fireEvent.click(getByTestId("create-cycle-button"), leftClick);
+
+      // See modal
+      await waitForElement(() => getByText(/let\'s setup your maxes/i));
+      expect(getByRole("button", { name: /setup/i })).toBeTruthy();
+
+      fireEvent.click(getByRole("button", { name: /setup/i }), leftClick);
+
+      // See form to enter maxes
+      const maxes = {
+        squat: 200,
+        bench: 150,
+        deadlift: 300,
+        press: 100,
+      };
+      for (const exercise in maxes) {
+        expect(
+          getByRole("textbox", { name: new RegExp(exercise, "i") })
+        ).toBeTruthy();
+      }
+
+      // Fill out maxes
+      for (const exercise in maxes) {
+        fireEvent.change(
+          getByRole("textbox", { name: new RegExp(exercise, "i") }),
+          { target: { value: maxes[exercise] } }
+        );
+      }
+      fireEvent.click(getByRole("button", { name: "submit" }), leftClick);
+
+      // See Summary
+      await waitForElement(() => getByText(/your maxes for the next cycle/i));
+      for (const exercise in maxes) {
+        expect(getByText(new RegExp(exercise, "i"))).toBeTruthy();
+        expect(getByText(new RegExp(maxes[exercise], "i"))).toBeTruthy();
+      }
+
+      // Click create
+      axios.post = jest.fn().mockImplementation((url) => {
+        switch (url) {
+          case "/api/cycles":
+            return Promise.resolve(res.postCycle);
+          case `/api/workouts/${res.postCycle.data.cycle._id}`:
+            return Promise.resolve(res.postWorkouts);
         }
+      });
+      axios.put = jest
+        .fn()
+        .mockImplementationOnce(() => Promise.resolve(res.putCycle));
+      fireEvent.click(getByRole("button", { name: "create" }), leftClick);
 
-        // Fill out maxes
-        for (const exercise in maxes) {
-            fireEvent.change(getByRole('textbox', {name: new RegExp(exercise,'i')}), {target: {value: maxes[exercise]}})
-        }
-        fireEvent.click(getByRole('button', {name: 'submit'}), leftClick);
-
-        // See Summary
-        await waitForElement(() => getByText(/your maxes for the next cycle/i))
-        for (const exercise in maxes) {
-            expect(getByText(new RegExp(exercise,'i'))).toBeTruthy();
-            expect(getByText(new RegExp(maxes[exercise],'i'))).toBeTruthy();
-        }
-
-        // Click create
-        axios.post = jest.fn()
-        .mockImplementation((url) => {
-          switch (url) {
-            case '/api/cycles':
-              return Promise.resolve(res.postCycle);
-            case `/api/workouts/${res.postCycle.data.cycle._id}`:
-              return Promise.resolve(res.postWorkouts)
-          }
-        })        
-        axios.put = jest.fn().mockImplementationOnce(() => Promise.resolve(res.putCycle))
-        fireEvent.click(getByRole('button', {name: 'create'}), leftClick);
-
-        // See new cycle is created
-        await waitForElement(() => getByText(res.postCycle.data.msg))
-        expect(getByRole('link', {name: 'workout'})).toBeTruthy();
-
-    })
-  })
-})
+      // See new cycle is created
+      await waitForElement(() => getByText(res.postCycle.data.msg));
+      expect(getByRole("link", { name: "workout" })).toBeTruthy();
+    });
+  });
+});
--- client/src/pages/__tests__/login.js
+++ client/src/pages/__tests__/login.js
@@ -1,137 +1,159 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect'
-import { screen } from '@testing-library/dom'
-import { render, fireEvent, waitForElement, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
-import axios from 'axios';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { screen } from "@testing-library/dom";
+import {
+  render,
+  fireEvent,
+  waitForElement,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import axios from "axios";
 
 // Components
-import App from '../../App';
+import App from "../../App";
 
-describe('Landing', () => {
-  describe('login to an account', () => {
+describe("Landing", () => {
+  describe("login to an account", () => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
 
-    Object.defineProperty(window, 'matchMedia', {
-        writable: true,
-        value: jest.fn().mockImplementation(query => ({
-          matches: false,
-          media: query,
-          onchange: null,
-          addListener: jest.fn(), // deprecated
-          removeListener: jest.fn(), // deprecated
-          addEventListener: jest.fn(),
-          removeEventListener: jest.fn(),
-          dispatchEvent: jest.fn(),
-        })),
-      });
-    
     const renderApp = () => {
-      let utils = render(<App/>)
-      return {...utils, history}
-    }
+      let utils = render(<App />);
+      return { ...utils, history };
+    };
 
     const res = {
       postAuthSuccess: {
-        data: 'asdofj1091jjksladf01asdf'
+        data: "asdofj1091jjksladf01asdf",
       },
       postAuthError: {
-          response: {
-              data: {
-                  error: {
-                      msg: 'Invalid Credentials'
-                  }
-              }
-          }
+        response: {
+          data: {
+            error: {
+              msg: "Invalid Credentials",
+            },
+          },
+        },
       },
       getAuth: {
         data: {
-          _id: '0s9adf808sadfa0sdf8',
-          name: 'fake',
-          email: 'fake@mail.com',
+          _id: "0s9adf808sadfa0sdf8",
+          name: "fake",
+          email: "fake@mail.com",
           cyclesCompleted: 0,
-        }
+        },
       },
       authError: {
         response: {
-                status: 500,
-                data: {
-                    error: {
-                        msg: 'Server Error'
-                    }
-                }
-        }
+          status: 500,
+          data: {
+            error: {
+              msg: "Server Error",
+            },
+          },
+        },
       },
       getCycles: {
         response: {
-            data: {
-                status: 400,
-                error: {
-                    msg: 'No cycles found'
-                }
-            }
-        }
+          data: {
+            status: 400,
+            error: {
+              msg: "No cycles found",
+            },
+          },
+        },
       },
-    }
+    };
 
-    beforeEach(() => jest.restoreAllMocks())
-    afterEach(() => jest.restoreAllMocks())
-  
-    test('login and redirect to dashboard', async () => {
-      axios.get = jest.fn().mockImplementationOnce(() => Promise.reject(res.authError))
+    beforeEach(() => jest.restoreAllMocks());
+    afterEach(() => jest.restoreAllMocks());
+
+    test("login and redirect to dashboard", async () => {
+      axios.get = jest
+        .fn()
+        .mockImplementationOnce(() => Promise.reject(res.authError));
 
       // Render Landing and Register Routes, start at Landing
-      const { getByText, getByTestId, getByPlaceholderText, getAllByPlaceholderText, getByRole, history } = renderApp();
+      const {
+        getByText,
+        getByTestId,
+        getByPlaceholderText,
+        getAllByPlaceholderText,
+        getByRole,
+        history,
+      } = renderApp();
 
       // See buttons to login or register
-      expect(getByRole('link', {name: /login/i})).toBeTruthy();
-      expect(getByRole('link', {name: /register/i})).toBeTruthy();
+      expect(getByRole("link", { name: /login/i })).toBeTruthy();
+      expect(getByRole("link", { name: /register/i })).toBeTruthy();
 
       // Navigate to Login
-      const leftClick = { button: 0 }
-      fireEvent.click(getByRole('link', {name: /login/i}), leftClick)
+      const leftClick = { button: 0 };
+      fireEvent.click(getByRole("link", { name: /login/i }), leftClick);
 
       // Arrived at Login page
-      expect(window.location.href).toContain('http://localhost/login');
+      expect(window.location.href).toContain("http://localhost/login");
       expect(getByText(/log into/i)).toBeTruthy();
-      
+
       // See login form
       expect(getByPlaceholderText(/email/i)).toBeTruthy();
       expect(getAllByPlaceholderText(/password/i)).toBeTruthy();
-      expect(getByRole('button', {name: /submit/i} )).toBeTruthy();
-      
+      expect(getByRole("button", { name: /submit/i })).toBeTruthy();
+
       // Fill form with invalid credentials
-      fireEvent.change(getByPlaceholderText(/email/i), {target: {value: 'fake@mail.com'}})
-      fireEvent.change(getByPlaceholderText(/password/i), {target: {value: 'wrongpassword'}})
-      
+      fireEvent.change(getByPlaceholderText(/email/i), {
+        target: { value: "fake@mail.com" },
+      });
+      fireEvent.change(getByPlaceholderText(/password/i), {
+        target: { value: "wrongpassword" },
+      });
+
       // Click sign up
-      axios.post = jest.fn().mockImplementationOnce(() => Promise.reject(res.postAuthError))
-      fireEvent.click(getByRole('button', {name: /submit/i} ), leftClick);
-      
+      axios.post = jest
+        .fn()
+        .mockImplementationOnce(() => Promise.reject(res.postAuthError));
+      fireEvent.click(getByRole("button", { name: /submit/i }), leftClick);
+
       // Password error alert appears
-      await waitForElement(()=>getByText(/invalid credentials/i));
+      await waitForElement(() => getByText(/invalid credentials/i));
 
       // Fill form with matching password
-      fireEvent.change(getByPlaceholderText(/password/i), {target: {value: 'correctpassword'}})
+      fireEvent.change(getByPlaceholderText(/password/i), {
+        target: { value: "correctpassword" },
+      });
 
       // Mock loaduser, getCycles
-      axios.get = jest.fn()
-      .mockImplementation((url) => {
-          switch(url) {
-              case '/api/auth':
-                  return Promise.resolve(res.getAuth)
-              case '/api/cycles':
-                  return Promise.reject(res.getCycles);
-          }
+      axios.get = jest.fn().mockImplementation((url) => {
+        switch (url) {
+          case "/api/auth":
+            return Promise.resolve(res.getAuth);
+          case "/api/cycles":
+            return Promise.reject(res.getCycles);
+        }
       });
       // mock loginUser
-      axios.post = jest.fn().mockImplementationOnce(() => Promise.resolve(res.postAuthSuccess))
-      
+      axios.post = jest
+        .fn()
+        .mockImplementationOnce(() => Promise.resolve(res.postAuthSuccess));
+
       // Click log in
-      fireEvent.click(getByRole('button', {name: /submit/i} ), leftClick);
+      fireEvent.click(getByRole("button", { name: /submit/i }), leftClick);
 
       // Redirected to dashboard
       await waitForElement(() => getByText(/logout/i));
-      expect(window.location.href).toContain('http://localhost/dashboard');
-
-    })
-  })
-})
+      expect(window.location.href).toContain("http://localhost/dashboard");
+    });
+  });
+});
