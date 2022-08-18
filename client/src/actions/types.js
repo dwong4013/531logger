75fc4f6b304cc661efdea7938cde0b546a3bae12
derrick <dwong4013@gmail.com>
2022-08-18 09:29:29 -0400

    reformatted action creators

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,27 +1,23 @@
-export const SET_ALERT = 'SET_ALERT';
-export const REMOVE_ALERT = 'REMOVE_ALERT';
-export const SET_TIMEOUT = 'SET_TIMEOUT';
-export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
-export const REGISTER_FAIL = 'REGISTER FAIL';
-export const USER_LOADED = 'USER_LOADED';
-export const AUTH_ERROR = 'AUTH_ERROR';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAIL = 'LOGIN_FAIL';
-export const LOGOUT = 'LOGOUT';
-export const DELETE_MAX = 'DELETE_MAX';
-export const GET_CYCLES = 'GET_CYCLES';
-export const ADD_CYCLE = 'ADD_CYCLE';
-export const NO_CYCLES = 'NO_CYCLES';
-export const UPDATE_CYCLE = 'UPDATE_CYCLE';
-export const DELETE_CYCLE = 'DELETE_CYCLE';
-export const CYCLE_ACTION_COMPLETE = 'CYCLE_ACTION_COMPLETE'
-export const CYCLE_ACTION_READY = 'CYCLE_ACTION_READY'
-export const CYCLE_ERROR = 'CYCLE_ERROR';
-export const GET_WORKOUT = 'GET_WORKOUT'
-export const NO_WORKOUT = 'NO_WORKOUT';
-export const UPDATE_WORKOUT = 'UPDATE_WORKOUT';
-export const WORKOUT_ERROR = 'WORKOUT_ERROR';
-
-
-
-
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
+export const SET_TIMEOUT = "SET_TIMEOUT";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER FAIL";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+export const DELETE_MAX = "DELETE_MAX";
+export const GET_CYCLES = "GET_CYCLES";
+export const ADD_CYCLE = "ADD_CYCLE";
+export const NO_CYCLES = "NO_CYCLES";
+export const UPDATE_CYCLE = "UPDATE_CYCLE";
+export const DELETE_CYCLE = "DELETE_CYCLE";
+export const CYCLE_ACTION_COMPLETE = "CYCLE_ACTION_COMPLETE";
+export const CYCLE_ACTION_READY = "CYCLE_ACTION_READY";
+export const CYCLE_ERROR = "CYCLE_ERROR";
+export const GET_WORKOUT = "GET_WORKOUT";
+export const NO_WORKOUT = "NO_WORKOUT";
+export const UPDATE_WORKOUT = "UPDATE_WORKOUT";
+export const WORKOUT_ERROR = "WORKOUT_ERROR";
--- client/src/actions/workouts.js
+++ client/src/actions/workouts.js
@@ -1,134 +1,132 @@
 import {
-    GET_WORKOUT,
-    NO_WORKOUT,
-    UPDATE_WORKOUT,
-    WORKOUT_ERROR,
-  } from './types';
-import { updateCycle } from './cycles';
-import { setAlert } from './alert';
-import setAuthToken from '../utils/setAuthToken';
-import axios from 'axios';
+  GET_WORKOUT,
+  NO_WORKOUT,
+  UPDATE_WORKOUT,
+  WORKOUT_ERROR,
+} from "./types";
+import { updateCycle } from "./cycles";
+import { setAlert } from "./alert";
+import setAuthToken from "../utils/setAuthToken";
+import axios from "axios";
 
-export const getWorkout = (cycleId, workoutId = null) => async dispatch => {
-  // Add token to headers
-  if (localStorage.token) {
-    setAuthToken(localStorage.token)
-  }
-  
-  try {
-    let res = await axios.get(`/api/workouts/${cycleId}`)
-    let targetWorkout
-    
-    if (workoutId) {
-      targetWorkout = res.data.find(exercise => exercise._id === workoutId)
-    } else {
-      // latest workout
-      targetWorkout = res.data.find(exercise => !exercise.completed )
+export const getWorkout =
+  (cycleId, workoutId = null) =>
+  async (dispatch) => {
+    // Add token to headers
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
     }
-    
-    // Update state with workout data
-    // always returns latest workout unless workoutId is specified
-    dispatch({
-      type: GET_WORKOUT,
-      payload: targetWorkout
-    })
-      
+
+    try {
+      let res = await axios.get(`/api/workouts/${cycleId}`);
+      let targetWorkout;
+
+      if (workoutId) {
+        targetWorkout = res.data.find((exercise) => exercise._id === workoutId);
+      } else {
+        // latest workout
+        targetWorkout = res.data.find((exercise) => !exercise.completed);
+      }
+
+      // Update state with workout data
+      // always returns latest workout unless workoutId is specified
+      dispatch({
+        type: GET_WORKOUT,
+        payload: targetWorkout,
+      });
     } catch (err) {
-      const statusCode = err.response.status
-      const error = err.response.data.error
+      const statusCode = err.response.status;
+      const error = err.response.data.error;
 
       // Update state with alert message and remove workout data
-      switch(statusCode) {
+      switch (statusCode) {
         case 400:
-          dispatch(setAlert('Error', error.msg, 'danger'));
-          dispatch({type: NO_WORKOUT});
+          dispatch(setAlert("Error", error.msg, "danger"));
+          dispatch({ type: NO_WORKOUT });
           break;
         case 500:
-          dispatch(setAlert('Error', error.msg, 'danger'))
+          dispatch(setAlert("Error", error.msg, "danger"));
           dispatch({ type: WORKOUT_ERROR });
           break;
         default:
           break;
       }
     }
+  };
 
-}
-
-export const createWorkouts = (cycleId) => async dispatch => {
+export const createWorkouts = (cycleId) => async (dispatch) => {
   // Add token to headers
   if (localStorage.token) {
-      setAuthToken(localStorage.token)
+    setAuthToken(localStorage.token);
   }
 
   try {
     const config = {
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     };
 
     const res = await axios.post(`/api/workouts/${cycleId}`, config);
 
     // Update state with alert
     if (res.statusCode === 400) {
-        dispatch(setAlert('Error', res.data.error.msg, 'danger'))
+      dispatch(setAlert("Error", res.data.error.msg, "danger"));
     }
 
     let cycleData = {
-        key: 'workoutsToDo',
-        values: res.data.workouts
-    }
+      key: "workoutsToDo",
+      values: res.data.workouts,
+    };
 
     // Update cycle with new workouts
-    dispatch(updateCycle(cycleId, cycleData))
-
+    dispatch(updateCycle(cycleId, cycleData));
   } catch (err) {
     const error = err.response.data.error;
 
     // Update state with alert
     if (error) {
-      dispatch(setAlert('Error', error.msg, 'danger'))
+      dispatch(setAlert("Error", error.msg, "danger"));
     }
   }
-    
-}
+};
 
-export const editWorkout = (workoutId, formData) => async dispatch => {
+export const editWorkout = (workoutId, formData) => async (dispatch) => {
   // Add token to headers
   if (localStorage.token) {
-      setAuthToken(localStorage.token)
+    setAuthToken(localStorage.token);
   }
 
   try {
     const config = {
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     };
 
     const res = await axios.put(`/api/workouts/${workoutId}`, formData, config);
 
     // Update state with new workout data
     dispatch({
-        type: UPDATE_WORKOUT,
-        payload: res.data.workout
+      type: UPDATE_WORKOUT,
+      payload: res.data.workout,
     });
   } catch (err) {
-    const statusCode = err.response.status
-    const error = err.response.data.error
-    
+    const statusCode = err.response.status;
+    const error = err.response.data.error;
+
     // Update state with alert message and remove workout data
-    switch(statusCode) {
+    switch (statusCode) {
       case 400:
-        dispatch(setAlert('Error', error.msg, 'danger'));
-        dispatch({type: NO_WORKOUT});
+        dispatch(setAlert("Error", error.msg, "danger"));
+        dispatch({ type: NO_WORKOUT });
         break;
       case 500:
-        dispatch(setAlert('Error', error.msg, 'danger'))
+        dispatch(setAlert("Error", error.msg, "danger"));
         dispatch({ type: WORKOUT_ERROR });
         break;
       default:
         break;
     }
   }
-}
+};
